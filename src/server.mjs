f1bd1d41cc2c7cd6b416fd880a31281b24ab4123
mimicarreira13 <mimicarreira13@hotmail.com>
2025-04-25 12:02:32 +0100

    feat(server): serve React build from Express for single-service deployment

--- src/server.mjs
+++ src/server.mjs
@@ -435,6 +435,13 @@ app.post('/posts', async (req, res) => {
     }
 });
 
+// Servir a build do React
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
